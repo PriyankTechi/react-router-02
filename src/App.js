c5a61ebf8e9ea493218a6b309d89500b9fe0e956
Priyank Gangwar <priyankgangwar.techi@gmail.com>
2022-09-14 15:14:10 +0530

    nested route

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetails from "./pages/QuoteDetails";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes"/>
-      </Route>
-      <Route path="/quotes" component={AllQuotes} exact />
-      <Route path="/quotes/:quoteId" component={QuoteDetails} />
-      <Route path="/new-quote" component={NewQuote} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" component={AllQuotes} exact />
+        <Route path="/quotes/:quoteId" component={QuoteDetails} />
+        <Route path="/new-quote" component={NewQuote} />
+      </Switch>
+    </Layout>
   );
 }
 
