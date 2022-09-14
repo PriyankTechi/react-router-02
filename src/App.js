651b1b8a0e77f464e5c1fd37c855484e1bde3bc9
Priyank Gangwar <priyankgangwar.techi@gmail.com>
2022-09-14 15:04:34 +0530

    useParam

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetails from "./pages/QuoteDetails";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes"/>
+      </Route>
+      <Route path="/quotes" component={AllQuotes} exact />
+      <Route path="/quotes/:quoteId" component={QuoteDetails} />
+      <Route path="/new-quote" component={NewQuote} />
+    </Switch>
   );
 }
 
---
+++ src/pages/NewQuote.js
@@ -0,0 +1,8 @@
+
+const NewQuote = () => {
+    return (
+        <h1>New Quote</h1>
+    );
+}
+
+export default NewQuote;
